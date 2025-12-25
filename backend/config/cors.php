b87256fa4722d0ee545efdb30b8afd4be8404776
subhakarb-babu <subhakarb.babu@gmail.com>
2025-12-25 21:10:23 +0530

    Fix CORS for Railway frontend

---
+++ backend/config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://frontend-production-b9b4.up.railway.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
